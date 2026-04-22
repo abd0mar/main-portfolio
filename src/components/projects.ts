f47f73ad6ed35936fb91f3aa6c47de986cd93744
abdo omar <aomar.stu@gmail.com>
2026-04-22 23:12:26 +0200

    Services Section

--- src/components/projects.ts
+++ src/components/projects.ts
@@ -22,36 +22,9 @@ export class Projects{
         technologies: ['HTML', 'CSS', 'JavaScript'],
         imageOne: dashboardDark,
         imageTwo: dashboardLight,
-        liveDemoLink: '',
-        githubLink: '',
+        liveDemoLink: 'https://abd0mar.github.io/Dashboard_Project/',
+        githubLink: 'https://github.com/abd0mar/Dashboard_Project.git',
       },
-      {
-        name: 'Dashboard Website',
-        description: 'A full-featured online store with product catalog, shopping cart, and secure checkout. Built with modern design principles and optimized for conversions.',
-        technologies: ['HTML', 'CSS', 'JavaScript'],
-        imageOne: dashboardDark,
-        imageTwo: dashboardLight,
-        liveDemoLink: '',
-        githubLink: '',
-      },
-      {
-        name: 'Dashboard Website',
-        description: 'A full-featured online store with product catalog, shopping cart, and secure checkout. Built with modern design principles and optimized for conversions.',
-        technologies: ['HTML', 'CSS', 'JavaScript'],
-        imageOne: dashboardDark,
-        imageTwo: dashboardLight,
-        liveDemoLink: '',
-        githubLink: '',
-      },
-      {
-        name: 'Dashboard Website',
-        description: 'A full-featured online store with product catalog, shopping cart, and secure checkout. Built with modern design principles and optimized for conversions.',
-        technologies: ['HTML', 'CSS', 'JavaScript'],
-        imageOne: dashboardDark,
-        imageTwo: dashboardLight,
-        liveDemoLink: '',
-        githubLink: '',
-      }
     ];
 
     this.init()
@@ -63,7 +36,9 @@ export class Projects{
   }
 
   renderProjects(){
-    if(this.projectsList.length > 3) this.isExtended = true;
+    if(this.projectsList.length > 3){
+      this.isExtended = true;
+    }
     this.projectsList.forEach((project: Project) => {
       const projectBox:HTMLDivElement = document.createElement("div");
       projectBox.className = 'project-card group';
@@ -81,11 +56,11 @@ export class Projects{
             ${project.description}
           </span>
           <div class="project-contact">
-            <a href="${project.liveDemoLink}" class="preview">
+            <a href="${project.liveDemoLink}"  target="_blank" class="preview">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link w-4 h-4" aria-hidden="true"><path d="M15 3h6v6"></path><path d="M10 14 21 3"></path><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path></svg>
               Live Demo
             </a>
-            <a href="${project.githubLink}" class="github-btn">
+            <a href="${project.githubLink}" target="_blank" class="github-btn">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github w-4 h-4" aria-hidden="true"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
             </a>
           </div>
@@ -111,17 +86,21 @@ export class Projects{
 
   showMoreBtnAction(){
     const showMoreBtn = document.querySelector("#show-more-btn");
-    showMoreBtn?.addEventListener("click", () => {
-      if(this.isExtended){
-        this.isExtended = false;
-        showMoreBtn.innerHTML = `<i class="fa-regular fa-eye-slash"></i>Show Less`;
-      }else{
-        this.isExtended = true
-        showMoreBtn.innerHTML = `<i class="fa-regular fa-eye"></i>Show More`;
-      }
-      this.expandAction()
-    })
-
+    if (this.projectsList.length > 3) {
+      showMoreBtn?.classList.remove("hidden");
+      showMoreBtn?.addEventListener("click", () => {
+        if(this.isExtended){
+          this.isExtended = false;
+          showMoreBtn.innerHTML = `<i class="fa-regular fa-eye-slash"></i>Show Less`;
+        }else{
+          this.isExtended = true
+          showMoreBtn.innerHTML = `<i class="fa-regular fa-eye"></i>Show More`;
+        }
+        this.expandAction()
+      })
+    }else{
+      showMoreBtn?.classList.add("hidden");
+    }
   }
 }
 

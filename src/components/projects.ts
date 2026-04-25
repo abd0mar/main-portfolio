1d69720b1d23a523b1927ec8562f0277f87b303e
Abdulrhman <aomar.stu@gmail.com>
2026-04-26 01:10:43 +0300

    Improving Performance

--- src/components/projects.ts
+++ src/components/projects.ts
@@ -2,21 +2,48 @@
 import dashboardDark from '../assets/images/project-dashboard-dark.webp';
 import dashboardLight from '../assets/images/project-dashboard-light.webp';
 
-export class Projects{
+export class Projects {
 
 
-  projectsSection: HTMLElement | null;
+  projectsContainer: HTMLElement | null;
   projectsList: Project[];
   isExtended: boolean;
-  projectSection:HTMLDivElement | null;
-  projectSectionContainer:HTMLDivElement | null;
+  projectSection: HTMLDivElement | null;
+  projectSectionContainer: HTMLDivElement | null;
 
-  constructor(){
-    this.projectsSection = document.querySelector('#projects-container');
-    this.projectSection = document.querySelector("#project-section");
-    this.projectSectionContainer= document.querySelector("#project-section-container");
+  constructor() {
+    this.projectsContainer = document.querySelector('#projects-container');
+    this.projectSection = document.querySelector("#projects-section");
+    this.projectSectionContainer = document.querySelector("#project-section-container");
     this.isExtended = false;
     this.projectsList = [
+      {
+        name: 'Dashboard Website',
+        description: 'A full-featured online store with product catalog, shopping cart, and secure checkout. Built with modern design principles and optimized for conversions.',
+        technologies: ['HTML', 'CSS', 'Vanilla JS'],
+        imageOne: dashboardDark,
+        imageTwo: dashboardLight,
+        liveDemoLink: 'https://abd0mar.github.io/Dashboard_Project/',
+        githubLink: 'https://github.com/abd0mar/Dashboard_Project.git',
+      },
+      {
+        name: 'Dashboard Website',
+        description: 'A full-featured online store with product catalog, shopping cart, and secure checkout. Built with modern design principles and optimized for conversions.',
+        technologies: ['HTML', 'CSS', 'Vanilla JS'],
+        imageOne: dashboardDark,
+        imageTwo: dashboardLight,
+        liveDemoLink: 'https://abd0mar.github.io/Dashboard_Project/',
+        githubLink: 'https://github.com/abd0mar/Dashboard_Project.git',
+      },
+      {
+        name: 'Dashboard Website',
+        description: 'A full-featured online store with product catalog, shopping cart, and secure checkout. Built with modern design principles and optimized for conversions.',
+        technologies: ['HTML', 'CSS', 'Vanilla JS'],
+        imageOne: dashboardDark,
+        imageTwo: dashboardLight,
+        liveDemoLink: 'https://abd0mar.github.io/Dashboard_Project/',
+        githubLink: 'https://github.com/abd0mar/Dashboard_Project.git',
+      },
       {
         name: 'Dashboard Website',
         description: 'A full-featured online store with product catalog, shopping cart, and secure checkout. Built with modern design principles and optimized for conversions.',
@@ -30,20 +57,20 @@ export class Projects{
 
     this.init()
   }
-  init(){
+  init() {
     this.renderProjects();
-    this.expandAction();
+    this.extendAction();
     this.showMoreBtnAction()
   }
 
-  renderProjects(){
-    if(this.projectsList.length > 3){
+  renderProjects() {
+    if (this.projectsList.length > 3) {
       this.isExtended = true;
     }
     this.projectsList.forEach((project: { name: string; description: string; technologies: string[]; imageOne: string; imageTwo: string; liveDemoLink: string; githubLink: string }, index: number) => {
-      const projectBox:HTMLDivElement = document.createElement("div");
+      const projectBox: HTMLDivElement = document.createElement("div");
       projectBox.className = 'project-card group';
-      projectBox.innerHTML =`
+      projectBox.innerHTML = `
         <div class="absolute flex items-center justify-center rounded-lg top-3 left-3 px-3 py-1 border border-border z-10 bg-bg-primary/90 backdrop-blur-2xl"><span class="font-bold text-text text-md">${this.projectsList.length < 10 ? "0" + (index + 1) : (index + 1)}</span></div>
         <div class="relative overflow-hidden">
           <img loading="lazy" class="relative group-hover:scale-105 delay-75" style="transition: 0.3s" src="${project.imageOne}" alt="${project.name} screenshot">
@@ -66,41 +93,42 @@ export class Projects{
             </a>
           </div>
         </div>`;
-      this.projectsSection?.append(projectBox);
+      this.projectsContainer?.append(projectBox);
     })
   }
 
-  expandAction(){
-    if(this.isExtended){
-      const fadeElement:HTMLDivElement | null = document.createElement("div");
-
-      fadeElement.id = "fade-element";
-      fadeElement.className = "absolute bottom-0 left-0 w-full h-60 bg-linear-to-t from-bg-alt via-bg-alt to-transparent z-10";
-      this.projectSection?.append(fadeElement);
-      this.projectSectionContainer?.classList.replace("max-h-full", "max-h-235");
-    }else{
-      const fadeElement:HTMLDivElement | null = document.querySelector("#fade-element");
-      fadeElement?.remove();
-      this.projectSectionContainer?.classList.replace("max-h-235", "max-h-full");
-    }
-  }
-
-  showMoreBtnAction(){
+  showMoreBtnAction() {
     const showMoreBtn = document.querySelector("#show-more-btn");
     if (this.projectsList.length > 3) {
       showMoreBtn?.classList.remove("hidden");
       showMoreBtn?.addEventListener("click", () => {
-        if(this.isExtended){
+        if (this.isExtended) {
           this.isExtended = false;
-          showMoreBtn.innerHTML = `<i class="fa-regular fa-eye-slash"></i>Show Less`;
-        }else{
+          showMoreBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 576 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M41-24.9c-9.4-9.4-24.6-9.4-33.9 0S-2.3-.3 7 9.1l528 528c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-96.4-96.4c2.7-2.4 5.4-4.8 8-7.2 46.8-43.5 78.1-95.4 93-131.1 3.3-7.9 3.3-16.7 0-24.6-14.9-35.7-46.2-87.7-93-131.1-47.1-43.7-111.8-80.6-192.6-80.6-56.8 0-105.6 18.2-146 44.2L41-24.9zM204.5 138.7c23.5-16.8 52.4-26.7 83.5-26.7 79.5 0 144 64.5 144 144 0 31.1-9.9 59.9-26.7 83.5l-34.7-34.7c12.7-21.4 17-47.7 10.1-73.7-13.7-51.2-66.4-81.6-117.6-67.9-8.6 2.3-16.7 5.7-24 10l-34.7-34.7zM325.3 395.1c-11.9 3.2-24.4 4.9-37.3 4.9-79.5 0-144-64.5-144-144 0-12.9 1.7-25.4 4.9-37.3L69.4 139.2c-32.6 36.8-55 75.8-66.9 104.5-3.3 7.9-3.3 16.7 0 24.6 14.9 35.7 46.2 87.7 93 131.1 47.1 43.7 111.8 80.6 192.6 80.6 37.3 0 71.2-7.9 101.5-20.6l-64.2-64.2z"/></svg>Show Less`;
+        } else {
           this.isExtended = true
-          showMoreBtn.innerHTML = `<i class="fa-regular fa-eye"></i>Show More`;
+          showMoreBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 576 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6-46.8 43.5-78.1 95.4-93 131.1-3.3 7.9-3.3 16.7 0 24.6 14.9 35.7 46.2 87.7 93 131.1 47.1 43.7 111.8 80.6 192.6 80.6s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1 3.3-7.9 3.3-16.7 0-24.6-14.9-35.7-46.2-87.7-93-131.1-47.1-43.7-111.8-80.6-192.6-80.6zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64-11.5 0-22.3-3-31.7-8.4-1 10.9-.1 22.1 2.9 33.2 13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-12.2-45.7-55.5-74.8-101.1-70.8 5.3 9.3 8.4 20.1 8.4 31.7z" /></svg>Show More`;
         }
-        this.expandAction()
+        this.extendAction()
       })
-    }else{
+    } else {
       showMoreBtn?.classList.add("hidden");
+    }
+  }
+
+  extendAction() {
+    if (this.isExtended) {
+      const fadeElement: HTMLDivElement | null = document.createElement("div");
+      console.log(fadeElement)
+
+      fadeElement.id = "fade-element";
+      fadeElement.className = "absolute bottom-0 left-0 w-full h-80 bg-linear-to-t from-bg-alt via-bg-alt to-transparent z-10";
+      this.projectSection?.append(fadeElement);
+      this.projectSectionContainer?.classList.add("max-h-200");
+    } else {
+      const fadeElement: HTMLDivElement | null = document.querySelector("#fade-element");
+      fadeElement?.remove();
+      this.projectSectionContainer?.classList.remove("max-h-200");
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import './style/style.css';
-import '@fortawesome/fontawesome-free/js/all.js';
 
 import { ThemeBtn } from './components/navbar';
 import { NavbarMovement } from './components/navbar';
